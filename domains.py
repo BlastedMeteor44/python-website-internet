f7090513dd1a0d6b392fc2e6d26dca4c64afd4a4
BlastedMeteor44 <65218875+ruhaan-bit@users.noreply.github.com>
2024-07-20 16:30:29 +0800

    Update and rename domains.txt to domains.py

---
+++ domains.py
@@ -0,0 +1,9 @@
+domains = []
+urls = []
+
+def addom(url, domain):
+  domains.append(domain)
+  urls.append(url)
+
+#adding urls
+addom("pwi://example.ex", "https://raw.githubusercontent.com/ruhaan-bit/python-website-internet/main/example.html")
